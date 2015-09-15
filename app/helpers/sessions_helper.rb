14c0163bc763afec64217244af7e4240b49d589b
Javier Rodriguez <javierorodriguez@gmail.com>
2015-09-15 22:38:58 +0300

    Reorder some methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,13 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -27,13 +34,6 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
-  end
-
-  # Forgets a persistent session.
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
   end
 
   # Logs out the current user.
